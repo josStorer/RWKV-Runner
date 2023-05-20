5883686003edf60991a7b39b822ce0db9337fdc9
josc146 <josStorer@outlook.com>
2023-05-20 15:33:38 +0800

    chore

--- backend-python/utils/ngrok.py
+++ backend-python/utils/ngrok.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 def ngrok_connect():
@@ -6,5 +7,5 @@ def ngrok_connect():
 
     conf.set_default(conf.PyngrokConfig(ngrok_path="./ngrok"))
     ngrok.set_auth_token(os.environ["ngrok_token"])
-    http_tunnel = ngrok.connect(8000)
+    http_tunnel = ngrok.connect(8000 if len(sys.argv) == 1 else int(sys.argv[1]))
     print(http_tunnel.public_url)
