a15d0db8aa03da8b62877edc874a488ff2b3e600
josc146 <josStorer@outlook.com>
2024-08-26 19:53:10 +0800

    format

--- frontend/src/pages/Downloads.tsx
+++ frontend/src/pages/Downloads.tsx
@@ -52,19 +52,29 @@ const Downloads: FC = observer(() => {
         <div className="flex flex-col gap-2 overflow-y-auto overflow-x-hidden p-1">
           {displayList.map((status, index) => {
             const downloadProgress = `${status.progress.toFixed(2)}%`
-            const downloadSpeed = `${status.downloading ? bytesToMb(status.speed) : '0'}MB/s`
+            const downloadSpeed = `${
+              status.downloading ? bytesToMb(status.speed) : '0'
+            }MB/s`
             let downloadDetails: string
             if (status.size < 1024 * 1024)
-              downloadDetails = `${bytesToKb(status.transferred) + 'KB'}/${bytesToKb(status.size) + 'KB'}`
+              downloadDetails = `${bytesToKb(status.transferred) + 'KB'}/${
+                bytesToKb(status.size) + 'KB'
+              }`
             else if (status.size < 1024 * 1024 * 1024)
-              downloadDetails = `${bytesToMb(status.transferred) + 'MB'}/${bytesToMb(status.size) + 'MB'}`
+              downloadDetails = `${bytesToMb(status.transferred) + 'MB'}/${
+                bytesToMb(status.size) + 'MB'
+              }`
             else
-              downloadDetails = `${bytesToGb(status.transferred) + 'GB'}/${bytesToGb(status.size) + 'GB'}`
+              downloadDetails = `${bytesToGb(status.transferred) + 'GB'}/${
+                bytesToGb(status.size) + 'GB'
+              }`
 
             return (
               <div className="flex flex-col gap-1" key={index}>
                 <Field
-                  label={`${status.downloading ? t('Downloading') + ': ' : ''}${status.name}`}
+                  label={`${status.downloading ? t('Downloading') + ': ' : ''}${
+                    status.name
+                  }`}
                   validationMessage={`${downloadProgress} - ${downloadDetails} - ${downloadSpeed} - ${status.url}`}
                   validationState={status.done ? 'success' : 'none'}
                 >
