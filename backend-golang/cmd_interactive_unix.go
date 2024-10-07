dac558002a5a53dacaa210d33a7800a6149adcbb
WangCe <halowang1991@gmail.com>
2024-10-07 13:06:25 +0800

    window cmd calling

--- backend-golang/cmd_interactive_unix.go
+++ backend-golang/cmd_interactive_unix.go
@@ -15,35 +15,33 @@ import (
 
 func (a *App) CmdInteractive(args []string, taskName string) error {
 	fmt.Println("🚧✅ CmdInteractive", args, taskName)
+
 	currentTime := time.Now().UnixMilli()
 	threadID := taskName + "_" + strconv.FormatInt(currentTime, 10)
+
 	wruntime.EventsEmit(a.ctx, "cmd_event")
-	// fmt.Println("0")
+
 	cmd := exec.Command(args[0], args[1:]...)
-	// fmt.Println("1")
+
 	stdout, err := cmd.StdoutPipe()
-	// fmt.Println("2")
 	if err != nil {
-		// fmt.Println(err)
 		return err
 	}
-	// fmt.Println("3")
+
 	cmd.Stderr = cmd.Stdout
-	// fmt.Println("4")
+
 	err = cmd.Start()
-	// fmt.Println("5")
+
 	if err != nil {
-		// fmt.Println(err)
 		return err
 	}
+
 	cmds[threadID] = args
 	cmdProcesses[threadID] = cmd.Process
-	// fmt.Println("6")
 	reader := bufio.NewReader(stdout)
+
 	for {
-		// fmt.Println("7")
 		line, _, err := reader.ReadLine()
-		// fmt.Println("8", line, err)
 		if err != nil {
 			delete(cmds, threadID)
 			delete(cmdProcesses, threadID)
@@ -51,13 +49,9 @@ func (a *App) CmdInteractive(args []string, taskName string) error {
 				wruntime.EventsEmit(a.ctx, "cmd_event", threadID)
 				return nil
 			}
-			// fmt.Println("9", err)
 			return err
 		}
-		// fmt.Println("10", line)
 		strLine := string(line)
-		// fmt.Println("11", strLine)
 		wruntime.EventsEmit(a.ctx, "cmd_event", threadID, strLine)
-		// fmt.Println("12")
 	}
 }
--- backend-golang/cmd_interactive_windows.go
+++ backend-golang/cmd_interactive_windows.go
@@ -8,62 +8,61 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+	"strconv"
 	"syscall"
+	"time"
 
 	wruntime "github.com/wailsapp/wails/v2/pkg/runtime"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
 )
 
-func (a *App) CmdInteractive(args []string, eventId string) error {
-	fmt.Println("1")
+func (a *App) CmdInteractive(args []string, taskName string) error {
+	fmt.Println("🚧✅ CmdInteractive", args, taskName)
+
+	currentTime := time.Now().UnixMilli()
+	threadID := taskName + "_" + strconv.FormatInt(currentTime, 10)
+
+	wruntime.EventsEmit(a.ctx, "cmd_event")
+
 	cmd := exec.Command(args[0], args[1:]...)
-	fmt.Println("2")
+
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Println("3")
 		return err
 	}
-	fmt.Println("4")
+
 	cmd.Stderr = cmd.Stdout
-	fmt.Println("5")
 	cmd.SysProcAttr = &syscall.SysProcAttr{}
 	cmd.SysProcAttr.HideWindow = true
+
 	err = cmd.Start()
+
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
-	fmt.Println("7")
-	cmds[eventId] = args
-	cmdProcesses[eventId] = cmd.Process
-	fmt.Println("8")
+
+	cmds[taskName] = args
+	cmdProcesses[taskName] = cmd.Process
 	reader := bufio.NewReader(stdout)
-	fmt.Println("9")
+
 	for {
 		line, _, err := reader.ReadLine()
-		fmt.Println("10")
 		if err != nil {
-			fmt.Println("11")
-			delete(cmds, eventId)
-			delete(cmdProcesses, eventId)
+			delete(cmds, taskName)
+			delete(cmdProcesses, taskName)
 			if err == io.EOF {
-				fmt.Println("12")
-				wruntime.EventsEmit(a.ctx, eventId+"-finish")
+				wruntime.EventsEmit(a.ctx, "cmd_event", threadID)
 				return nil
 			}
 			return err
 		}
-		fmt.Println("13")
 		reader := transform.NewReader(bytes.NewReader(line), simplifiedchinese.GBK.NewDecoder())
-		fmt.Println("14")
 		line2, err := io.ReadAll(reader)
 		if err == nil {
 			line = line2
 		}
-		fmt.Println("15")
 		strLine := string(line)
-		fmt.Println("16")
-		wruntime.EventsEmit(a.ctx, eventId+"-output", strLine)
+		wruntime.EventsEmit(a.ctx, "cmd_event", threadID, strLine)
 	}
 }
