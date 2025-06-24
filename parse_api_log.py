03812085b6a470926a122b4bd1e725d6d0e3ec50
josc146 <josStorer@outlook.com>
2025-06-24 23:44:34 +0800

    format

--- parse_api_log.py
+++ parse_api_log.py
@@ -5,31 +5,37 @@ import sys
 def extract_data(log_file):
     entries = []
 
-    with open(log_file, 'r', encoding="utf-8") as file:
+    with open(log_file, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
         for i, line in enumerate(lines):
-            if line.startswith('Generation Prompt:') and not lines[i + 1].startswith("<pad>"):
-                current_entry = {'prompt': "", 'response': ""}
+            if line.startswith("Generation Prompt:") and not lines[i + 1].startswith(
+                "<pad>"
+            ):
+                current_entry = {"prompt": "", "response": ""}
 
                 prompt_end_point = i + 1
                 for j in range(i + 1, len(lines)):
-                    if lines[j].strip().endswith('- INFO'):
-                        current_entry['prompt'] = current_entry['prompt'].rstrip()
+                    if lines[j].strip().endswith("- INFO"):
+                        current_entry["prompt"] = current_entry["prompt"].rstrip()
                         break
-                    current_entry['prompt'] += lines[j]
+                    current_entry["prompt"] += lines[j]
                     prompt_end_point = j
 
                 for j in range(prompt_end_point + 1, len(lines)):
-                    if lines[j].startswith('Url:') and lines[j].strip().endswith("/completions"):
+                    if lines[j].startswith("Url:") and lines[j].strip().endswith(
+                        "/completions"
+                    ):
                         for k in range(j + 1, len(lines)):
-                            if lines[k].startswith('Data:'):
+                            if lines[k].startswith("Data:"):
                                 for l in range(k + 1, len(lines)):
                                     if "RequestsNum: " in lines[l]:
-                                        current_entry['response'] = current_entry['response'].rstrip()
+                                        current_entry["response"] = current_entry[
+                                            "response"
+                                        ].rstrip()
                                         entries.append(current_entry)
                                         break
-                                    current_entry['response'] += lines[l]
+                                    current_entry["response"] += lines[l]
                                 else:
                                     continue
                                 break
@@ -40,27 +46,28 @@ def extract_data(log_file):
 
 
 def main():
-    log_file = 'D:\\RWKV_Runner\\api.log' if len(sys.argv) < 2 else sys.argv[1]
+    log_file = "D:\\RWKV_Runner\\api.log" if len(sys.argv) < 2 else sys.argv[1]
     entries = extract_data(log_file)
 
     try:
         import cyac
+
         trie = cyac.Trie()
         histories = []
         for entry in entries:
-            v = entry['prompt'] + entry['response']
+            v = entry["prompt"] + entry["response"]
             trie.insert(v)
         for entry in entries:
-            v = entry['prompt'] + entry['response']
+            v = entry["prompt"] + entry["response"]
             for id in trie.predict(v):
                 pass
             if trie[id] == v:
                 histories.append(entry)
         json_data = json.dumps(histories, indent=2)
-    except ModuleNotFoundError:
+    except (ModuleNotFoundError, AttributeError):
         json_data = json.dumps(entries, indent=2)
 
-    print(json_data.encode('utf-8').decode('unicode_escape'))
+    print(json_data.encode("utf-8").decode("unicode_escape"))
 
 
 if __name__ == "__main__":
